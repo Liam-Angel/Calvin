63ea2fc9206dee991db8236b00268b854efe4d35
Liam Angel <0622557@schoolsnet.act.edu.au>
2026-02-26 13:41:41 +1100

    Test

--- Calvin/Calvin/settings.py
+++ Calvin/Calvin/settings.py
@@ -1,6 +1,8 @@
 """
 Django settings for Calvin project.
 
+t=Test
+
 Based on by 'django-admin startproject' using Django 2.1.2.
 
 For more information on this file, see
@@ -37,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'MyApp1',
 ]
 
 # Middleware framework
---
+++ Calvin/MyApp1/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from datetime import datetime
+from .models import teacher
+
+# Create your views here.
+def index(request):
+   teach = teacher.objects.all
+   return render(request,"MyApp1/index.html",{'content': teach})
