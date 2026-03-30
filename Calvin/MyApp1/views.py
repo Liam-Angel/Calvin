a0075678560da50af97129151a950564b9b0dd8b
Liam Angel <0622557@schoolsnet.act.edu.au>
2026-03-31 10:13:43 +1100

    im trying

--- Calvin/MyApp1/views.py
+++ Calvin/MyApp1/views.py
@@ -1,3 +1,4 @@
+from asyncio.windows_events import NULL
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from datetime import datetime
@@ -6,29 +7,40 @@ from .models import twin
 from .models import student
 from .forms import InputForm
 
+removals = []
+
+
 # Create your views here.
 def index(request):
    twins = twin.objects.all
    teachers = teacher.objects.all
    students = student.objects.all
-   return render(request,"MyApp1/index.html",{'content2': twins, 'content': teachers, 'content3': students})
+   return render(request,"MyApp1/index.html",{'content': teachers})
 
 def input_view(request):
     teachers = teacher.objects.all
-
+   
     if 'submit' in request.POST:
         form = InputForm(request.POST)
         if form.is_valid():
             form.save()
-    if 'delete' in request.POST:
-        id = request.POST.get("delete")
+    if 'select' in request.POST:
+        id = request.POST.get("select")
         if teacher.objects.filter(id=id).exists():
-            entry = teacher.objects.get(id=id)
-            entry.delete()
+            removals.append(id)
+            
+    if 'delete' in request.POST:
+        for item in removals:
+            if teacher.objects.filter(id=item).exists():
+                entry = teacher.objects.get(id=item)
+                entry.delete()
+                removals.clear
+
+            
 
     if 'back' in request.POST:
         return redirect("index")
     else:
         form = InputForm()
-        return render(request, "MyApp1/input.html",{'form': form, 'content': teachers})
+        return render(request, "MyApp1/input.html",{'form': form, 'content': teachers, 'removals': removals})
 
