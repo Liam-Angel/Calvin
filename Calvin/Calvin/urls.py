56f28fd3996f98b59fd7a18138c7764a3fd5c97e
Liam Angel <0622557@schoolsnet.act.edu.au>
2026-05-05 10:17:50 +1000

    pdf now

--- Calvin/Calvin/urls.py
+++ Calvin/Calvin/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
     re_path(r'^$', MyApp1.views.index, name='index'),
     re_path(r'^home$', MyApp1.views.index, name='index'),
     re_path(r'input', MyApp1.views.input_view, name='input'),
-    re_path(r'pdfr', MyApp1.views.pdfr_view, name='pdfr')
+    re_path(r'pdfr', MyApp1.views.pdfr_view, name='pdfr'),
+    path('report/', MyApp1.views.report, name='report'),
 
 ]
--- Calvin/MyApp1/views.py
+++ Calvin/MyApp1/views.py
@@ -3,12 +3,18 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from datetime import datetime
 from pypdf import PdfReader
+from pypdf import PdfWriter
+from reportlab.pdfgen import canvas
 from .models import teacher
 from .models import twin
 from .models import PDF
 from .forms import InputForm
 from .forms import pdfr
-
+from .forms import pdfr
+from reportlab.platypus import Paragraph,Image,Table 
+from django.http import FileResponse 
+from django.contrib.staticfiles.storage import staticfiles_storage 
+from io import BytesIO
 
 removals = []
 
@@ -46,9 +52,9 @@ def input_view(request):
 
 def pdfr_view(request):
     PDFs = PDF.objects.all
-
+    page
     if 'submit' in request.POST:
-        reader = PdfReader("meth.pdf")
+        reader = PdfReader("H:\Programming\Calvin\Calvin\MyApp1\meth.pdf")
         page = reader.pages[0]
         print(page.extract_text())
              
@@ -56,5 +62,46 @@ def pdfr_view(request):
         return redirect("index")
     else:
 
-        return render(request, "MyApp1/pdfr.html",{})
+        return render(request, "MyApp1/pdfr.html",{'page',page.extract_text() })
 
+
+def generate_pdf_file():
+    buffer = BytesIO()
+    p = canvas.Canvas(buffer)
+    lines = [('Name:', 'Teaching Area:')]
+    teachers = teacher.objects.all()
+
+    for teach in teachers:
+        lines.append((teach.Name, teach.Area))
+
+    table = Table(lines)
+    table.wrapOn(p, 300, 300)
+    table.drawOn(p, 10, 650)
+
+    p.showPage()
+    p.save()
+
+    buffer.seek(0)
+    return buffer
+
+def report(request):
+    pdf_file =  staticfiles_storage.path("meth.pdf")
+
+    try:
+        merger = PdfWriter()
+        input1 = PdfReader(generate_pdf_file())
+        input2 = PdfReader(pdf_file, "rb")
+
+        merger.append(input1)
+        merger.append(input2)
+
+        buffer = BytesIO()
+        merger.write(buffer)
+        buffer.seek(0)
+        response = FileResponse(buffer, as_attachment=True, filename="hello.pdf")
+
+    except FileNotFoundError:
+
+        response = FileResponse(generate_pdf_file(), as_attachment=True, filename="no.pdf")
+
+    return response
